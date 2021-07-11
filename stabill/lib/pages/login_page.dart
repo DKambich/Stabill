fa31267808e8d7909ff0967aa56942c07f66551a
Daniel Kambich <daniel.kambich@gmail.com>
2021-07-11 16:26:02 -0500

    Add basic login form

--- stabill/lib/pages/login_page.dart
+++ stabill/lib/pages/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:stabill/pages/home_page.dart';
 
@@ -13,9 +12,59 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+
   @override
-  void initState() {
+  initState() {
+    emailController = new TextEditingController();
+    passwordController = new TextEditingController();
     super.initState();
+  }
+
+  String? emailValidator(String? value) {
+    if (value == null) {
+      return null;
+    }
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value)) {
+      return 'Email format is invalid';
+    } else {
+      return null;
+    }
+  }
+
+  String? passwordValidator(String? value) {
+    if (value == null) {
+      return null;
+    }
+    if (value.length < 6) {
+      return 'Password must be longer than 6 characters';
+    } else {
+      return null;
+    }
+  }
+
+  void login() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text,
+          password: passwordController.text,
+        );
+
+        Navigator.pushReplacementNamed(
+          context,
+          HomePage.routeName,
+        );
+      } on FirebaseAuthException catch (e) {
+        print('Failed with error code: ${e.code}');
+        print(e.message);
+      }
+    }
   }
 
   @override
@@ -25,45 +74,41 @@ class _LoginPageState extends State<LoginPage> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(40.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    hintText: "someone@example.com",
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      hintText: "someone@example.com",
+                    ),
+                    validator: emailValidator,
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Password",
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                    ),
+                    validator: passwordValidator,
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: ElevatedButton(
-                  child: Text("Login"),
-                  onPressed: () {
-                    FirebaseAuth.instance
-                        .signInWithEmailAndPassword(
-                            email: "daniel.kambich@gmail.com",
-                            password: "123456")
-                        .then(
-                          (value) => Navigator.pushReplacementNamed(
-                            context,
-                            HomePage.routeName,
-                          ),
-                        );
-                  },
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: ElevatedButton(
+                    child: Text("Login"),
+                    onPressed: login,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
