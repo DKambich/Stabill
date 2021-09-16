ffed56d1453dc96f912ad412641bf23ba05be10a
Daniel Kambich <daniel.kambich@gmail.com>
2021-09-16 17:06:58 -0500

    Add rudimentary file importing

--- stabill/lib/pages/home_page.dart
+++ stabill/lib/pages/home_page.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:stabill/models/account.dart';
 import 'package:stabill/pages/login_page.dart';
 import 'package:stabill/widgets/modals/create_account_modal.dart';
 import 'package:stabill/widgets/account_list.dart';
 import 'package:stabill/widgets/modals/transfer_funds_modal.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:stabill/models/transaction.dart' as Stabill;
 
 class HomePage extends StatefulWidget {
   static final String routeName = "/home";
@@ -39,11 +45,102 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
         actions: [
-          PopupMenuButton(onSelected: (HomePageAction selected) {
+          PopupMenuButton(onSelected: (HomePageAction selected) async {
             switch (selected) {
-              case HomePageAction.Export:
-                break;
               case HomePageAction.Import:
+                FilePickerResult? result = await FilePicker.platform.pickFiles(
+                  allowedExtensions: ['csv'],
+                  type: FileType.custom,
+                  allowMultiple: false,
+                );
+                if (result != null) {
+                  File file = File(result.files[0].path!);
+
+                  final lines = await file.readAsLines();
+
+                  List<String> headers = lines[0].split(",");
+                  print(headers);
+                  lines.removeAt(0);
+
+                  Map<String, int> headerIndex = headers
+                      .asMap()
+                      .map<String, int>((key, value) => MapEntry(value, key));
+
+                  Map<String, List<Stabill.Transaction>> accounts =
+                      Map<String, List<Stabill.Transaction>>();
+
+                  for (String line in lines) {
+                    final entries = line.split(",");
+                    Stabill.Transaction transaction = Stabill.Transaction(
+                      name: entries[headerIndex["Transaction_Name"]!],
+                      amount: int.parse(entries[headerIndex["Amount"]!]) / 100,
+                      checkNumber: int.parse(entries[headerIndex["Check_No"]!]),
+                      cleared:
+                          entries[headerIndex["Has_Cleared"]!].toLowerCase() ==
+                              "true",
+                      method: entries[headerIndex["Transaction_Type"]!]
+                                  .toLowerCase() ==
+                              "true"
+                          ? Stabill.TransactionType.Deposit
+                          : Stabill.TransactionType.Withdrawal,
+                      timestamp: DateTime.fromMillisecondsSinceEpoch(
+                          int.parse(entries[headerIndex["Creation_Date"]!])),
+                      hidden:
+                          entries[headerIndex["Is_Hidden"]!].toLowerCase() ==
+                              "true",
+                      memo: entries[headerIndex["Memo"]!],
+                    );
+
+                    var list = accounts.putIfAbsent(
+                      entries[headerIndex["Account_Name"]!],
+                      () => [],
+                    );
+                    list.add(transaction);
+                  }
+
+                  // Initialize Firebase variables
+                  String uid = FirebaseAuth.instance.currentUser!.uid;
+                  var _accountsCollection = FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(uid)
+                      .collection("accounts")
+                      .withConverter<Account>(
+                        fromFirestore: (snapshot, _) =>
+                            Account.fromJson(snapshot.data()!),
+                        toFirestore: (acc, _) => acc.toJson(),
+                      );
+
+                  for (String key in accounts.keys) {
+                    Account newAccount = Account(name: key);
+                    var accountRef = await _accountsCollection.add(newAccount);
+                    List<Stabill.Transaction> transactions = accounts[key]!;
+                    List<List<Stabill.Transaction>> sublists = [];
+                    for (int i = 0; i < transactions.length; i += 499) {
+                      sublists.add(transactions.sublist(
+                          i,
+                          i + 499 > transactions.length
+                              ? transactions.length
+                              : i + 499));
+                    }
+                    var transactionRef = accountRef
+                        .collection("transactions")
+                        .withConverter<Stabill.Transaction>(
+                          fromFirestore: (snapshot, _) =>
+                              Stabill.Transaction.fromJson(snapshot.data()!),
+                          toFirestore: (acc, _) => acc.toJson(),
+                        );
+                    List<WriteBatch> batches = [];
+                    for (var list in sublists) {
+                      WriteBatch batch = FirebaseFirestore.instance.batch();
+                      for (var t in list)
+                        batch.set<Stabill.Transaction>(transactionRef.doc(), t);
+                      batches.add(batch);
+                    }
+                    await Future.wait(batches.map((e) => e.commit()));
+                  }
+                }
+                break;
+              case HomePageAction.Export:
                 break;
               case HomePageAction.Settings:
                 break;
@@ -60,7 +157,7 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               const PopupMenuItem<HomePageAction>(
-                value: HomePageAction.Import,
+                value: HomePageAction.Export,
                 child: ListTile(
                   leading: Icon(Icons.file_upload_outlined),
                   title: Text("Export Data"),
