650902a5d53ab8834961c3b02ae10447cb8937ba
Daniel Kambich <daniel.kambich@gmail.com>
2025-02-14 18:26:41 -0600

    Add basic router redirect logic

--- stabill/lib/config/router.dart
+++ stabill/lib/config/router.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:stabill/providers/auth_provider.dart';
 import 'package:stabill/ui/pages/auth/sign_in_page.dart';
 import 'package:stabill/ui/pages/home/home_page.dart';
 
-final goRouter = GoRouter(routes: _routes);
+final goRouter = GoRouter(
+  routes: _routes,
+  redirect: (BuildContext context, GoRouterState state) {
+    if (!context.read<AuthProvider>().isLoggedIn) {
+      return '/sign-in';
+    } else {
+      return null;
+    }
+  },
+);
 
 final _routes = [
   GoRoute(
