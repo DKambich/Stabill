247a4c6a2cd1867809b787694eb14b54237533ef
Daniel Kambich <daniel.kambich@gmail.com>
2025-02-28 17:06:28 -0600

    Utilize accounts stream in demo page

--- stabill/lib/ui/pages/home/home_page.dart
+++ stabill/lib/ui/pages/home/home_page.dart
@@ -5,6 +5,8 @@ import 'package:provider/provider.dart';
 import 'package:stabill/core/classes/result.dart';
 import 'package:stabill/core/services/account/account_service.dart';
 import 'package:stabill/core/services/navigation/navigation_service.dart';
+import 'package:stabill/data/models/account.dart';
+import 'package:stabill/data/models/balance.dart';
 import 'package:stabill/providers/auth_provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,6 +19,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Stream<List<Account>>? accountsStream;
+  Stream<Balance>? totalBalanceStream;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,10 +45,42 @@ class _HomePageState extends State<HomePage> {
               ElevatedButton(
                 onPressed: _createAccount,
                 child: Text('Create Account'),
-              )
+              ),
+              ElevatedButton(
+                onPressed: _listenToTotalBalance,
+                child: Text('Listen to Balance'),
+              ),
+              StreamBuilder(
+                stream: totalBalanceStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return SelectableText(snapshot.data.toString());
+                  }
+                  return SelectableText(snapshot.connectionState.toString());
+                },
+              ),
+              StreamBuilder(
+                stream: accountsStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: snapshot.data!
+                          .map((account) => SelectableText(account.toString()))
+                          .toList(),
+                    );
+                  }
+                  return Text('');
+                },
+              ),
             ],
           ),
         ));
+  }
+
+  @override
+  void initState() {
+    accountsStream = context.read<AccountService>().getAccounts();
+    super.initState();
   }
 
   void _createAccount() async {
@@ -54,6 +91,12 @@ class _HomePageState extends State<HomePage> {
     if (data != null) {
       log(data.toString());
     }
+  }
+
+  void _listenToTotalBalance() {
+    setState(() {
+      totalBalanceStream = context.read<AccountService>().getTotalBalance();
+    });
   }
 
   void _logout() async {
