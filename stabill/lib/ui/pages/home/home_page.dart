ee848098c0f44bc85b89bb8bdbe953b9460c3f62
Daniel Kambich <daniel.kambich@gmail.com>
2025-03-15 21:23:29 -0500

    Cleanup homepage

--- stabill/lib/ui/pages/home/home_page.dart
+++ stabill/lib/ui/pages/home/home_page.dart
@@ -1,13 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:stabill/core/classes/result.dart';
 import 'package:stabill/core/services/account/account_service.dart';
 import 'package:stabill/core/services/navigation/navigation_service.dart';
-import 'package:stabill/data/models/account.dart';
 import 'package:stabill/data/models/balance.dart';
 import 'package:stabill/providers/auth_provider.dart';
+import 'package:stabill/ui/widgets/balance_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -19,61 +16,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Stream<List<Account>>? accountsStream;
-  Stream<Balance>? totalBalanceStream;
+  Stream<Balance>? balanceStream;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          // Here we take the value from the SignInPage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text("Home Page"),
         ),
         body: Center(
           child: Column(
-            spacing: 24,
             children: [
+              Card(
+                child: GestureDetector(
+                  onTap: _goToAccounts,
+                  child: StreamBuilder<Balance>(
+                      stream: balanceStream,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Text("Loading");
+                        }
+
+                        if (!snapshot.hasData) {
+                          return Text(snapshot.connectionState.toString());
+                        }
+
+                        var balance = snapshot.data!;
+
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              const Text(
+                                "Accounts",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Column(
+                                    children: [
+                                      BalanceText(
+                                        balance: balance.availableInDollars,
+                                      ),
+                                      const Text("Available")
+                                    ],
+                                  ),
+                                  Column(
+                                    children: [
+                                      BalanceText(
+                                        balance: balance.currentInDollars,
+                                      ),
+                                      const Text("Current")
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        );
+                      }),
+                ),
+              ),
               ElevatedButton(
                 onPressed: _logout,
                 child: Text('Logout'),
-              ),
-              ElevatedButton(
-                onPressed: _createAccount,
-                child: Text('Create Account'),
-              ),
-              ElevatedButton(
-                onPressed: _listenToTotalBalance,
-                child: Text('Listen to Balance'),
-              ),
-              ElevatedButton(
-                onPressed: _goToAccounts,
-                child: Text('Go To Accounts'),
-              ),
-              StreamBuilder(
-                stream: totalBalanceStream,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return SelectableText(snapshot.data.toString());
-                  }
-                  return SelectableText(snapshot.connectionState.toString());
-                },
-              ),
-              StreamBuilder(
-                stream: accountsStream,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Column(
-                      children: snapshot.data!
-                          .map((account) => SelectableText(account.toString()))
-                          .toList(),
-                    );
-                  }
-                  return Text('');
-                },
               ),
             ],
           ),
@@ -82,28 +95,12 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    accountsStream = context.read<AccountService>().getAccounts();
+    balanceStream = context.read<AccountService>().getTotalBalance();
     super.initState();
   }
 
-  void _createAccount() async {
-    var Result(:data) = await context
-        .read<AccountService>()
-        .createAccount("Account Name", 12345);
-
-    if (data != null) {
-      log(data.toString());
-    }
-  }
-
-  _goToAccounts() {
+  void _goToAccounts() {
     context.read<NavigationService>().navigateToAccounts();
-  }
-
-  void _listenToTotalBalance() {
-    setState(() {
-      totalBalanceStream = context.read<AccountService>().getTotalBalance();
-    });
   }
 
   void _logout() async {
