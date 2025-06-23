d5530b559070ad03e747bc2fe3032f013250e948
Daniel Kambich <daniel.kambich@gmail.com>
2025-06-23 08:14:55 -0500

    Create basic transaction service and method

---
+++ stabill/lib/core/services/transaction/transaction_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:stabill/core/classes/result.dart';
+import 'package:stabill/data/models/transaction.dart';
+import 'package:stabill/data/repository/abstract_database_repository.dart';
+
+/// A service responsible for managing user accounts.
+class TransactionService {
+  final AbstractDatabaseRepository _databaseRepository;
+
+  TransactionService(this._databaseRepository);
+
+  Future<Result<List<Transaction>>> getTransactions(String accountId) async {
+    try {
+      var transactions = await _databaseRepository.getTransactions(accountId);
+      return Result.success(transactions);
+    } catch (error, stackTrace) {
+      debugPrint("getAccount() failed: $error\n$stackTrace");
+      return Result.failure(error);
+    }
+  }
+}
